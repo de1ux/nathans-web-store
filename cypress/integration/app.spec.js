5c7b1f05f9b0d53605e329a1012ae57e5bf46442
de1ux <nathan@de1ux.com>
2021-08-01 18:09:50 -0600

    Remove cypress

--- cypress/integration/app.spec.js
+++
@@ -1,6 +0,0 @@
-describe("Nathans web store", () => {
-    it("should add some items to the cart", () => {
-        cy.visit("https://google.com")
-        cy.get("[name=q]").type("Cat pictures").type("{enter}")
-    })
-})
