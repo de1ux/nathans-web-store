a61726fe17a0f260774c0d024b7d210971fa1f0e
de1ux <nathan@de1ux.com>
2021-08-10 17:51:24 -0600

    Changes to use auth0

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import './App.css';
 import {useEffect, useState} from "react";
 import {calculateTotal} from "./pricing";
 import axios from "axios";
+import {useAuth0} from "@auth0/auth0-react";
 
 
 function App() {
     const [items, setItems] = useState([]);
     const [cartItems, setCartItems] = useState([]);
+    const [idToken, setIdToken] = useState();
     const [total, setTotal] = useState();
+
+    const {user, isAuthenticated, isLoading, loginWithRedirect, logout, getIdTokenClaims} = useAuth0();
 
     useEffect(async () => {
         const data = await axios.get("https://nathans-backend-store.herokuapp.com/items")
             .then(response => response.data.items)
         setItems(data);
+
+        if (isAuthenticated) {
+            getIdTokenClaims().then(d => setIdToken(d.__raw))
+        }
     }, [])
 
     const addToCart = (item) => {
@@ -27,35 +35,55 @@ function App() {
         setTotal(calculateTotal(cartItems));
     }
 
+    if (isLoading) {
+        return <p>Loading...</p>
+    }
 
     return (
         <div>
-            <h1>Nathan's Amazing Web Store</h1>
+            {isAuthenticated ?
+                <div>
+                    <h1>Nathan's Amazing Web Store</h1>
 
-            <div>
-                <button data-cy="calculate-total" onClick={onCalculateTotal}>Calculate total</button>
-                {
-                    total ? <p>Total is: {total}</p> : null
-                }
-                <p>Cart: 0</p>
-                <ul>
-                    {cartItems.map((item, idx) =>
-                        <li key={idx}>
-                            {item.name} &nbsp;
-                            <button onClick={(e) => removeFromCart(item)}>Remove</button>
-                        </li>
-                    )}
-                </ul>
-            </div>
+                    <p>{JSON.stringify(user)}</p>
 
-            <div className="inventory-items" style={{display: "flex", flexWrap: "wrap"}}>
-                {items.map((item, idx) =>
-                    <div key={idx}>
-                        <p>{item.name} | ${item.price}</p>
-                        <button onClick={(e) => addToCart(item)}>Add to cart</button>
+                    <h3>ID token</h3>
+                    <p>{JSON.stringify(idToken)}</p>
+
+                    <img src={user['picture']}/>
+
+                    <button onClick={(event) => logout()}>Logout</button>
+
+                    <div>
+                        <button data-cy="calculate-total" onClick={onCalculateTotal}>Calculate total</button>
+                        {
+                            total ? <p>Total is: {total}</p> : null
+                        }
+                        <p>Cart: 0</p>
+                        <ul>
+                            {cartItems.map((item, idx) =>
+                                <li key={idx}>
+                                    {item.name} &nbsp;
+                                    <button onClick={(e) => removeFromCart(item)}>Remove</button>
+                                </li>
+                            )}
+                        </ul>
                     </div>
-                )}
-            </div>
+
+                    <div className="inventory-items" style={{display: "flex", flexWrap: "wrap"}}>
+                        {items.map((item, idx) =>
+                            <div key={idx}>
+                                <p>{item.name} | ${item.price}</p>
+                                <button onClick={(e) => addToCart(item)}>Add to cart</button>
+                            </div>
+                        )}
+                    </div>
+                </div>
+                 :
+                <div>
+                    <button onClick={((event) => loginWithRedirect())}>Login!</button>
+                </div>
+        }
         </div>
     );
 }
--- src/index.js
+++ src/index.js
@@ -3,10 +3,18 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import {Auth0Provider} from "@auth0/auth0-react";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <Auth0Provider
+          domain="dev-lifxamti.auth0.com"
+          clientId="S04lE3s4QG7DkZ0ECFqlGh5B8J21PrCk"
+          redirectUri={window.location.origin}
+          scope="read:current_user"
+      >
+        <App />
+      </Auth0Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
